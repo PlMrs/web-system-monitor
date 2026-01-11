d6a38cef7cb3e79faab574ec418ac86ba96b1366
PlMrs <paulmaraispro@gmail.com>
2026-01-12 00:40:30 +0100

    fix script not loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import { headers } from "next/headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,13 +19,14 @@ export const metadata: Metadata = {
   description: "Monitoring système",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const nonce = (await headers()).get("x-nonce") || "";
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" nonce={nonce}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

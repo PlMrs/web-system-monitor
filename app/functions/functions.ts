07c0d8feef8a61f9713cf88cd8fbea8fa08ca953
PlMrs <paulmaraispro@gmail.com>
2026-01-12 00:25:05 +0100

    update CSP

--- app/functions/functions.ts
+++ app/functions/functions.ts
@@ -36,9 +36,9 @@ export const computeCSSHeader: TComputeCSSHeader = (
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
+    script-src 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'unsafe-inline' ;
+    img-src 'self' blob: data: https://authjs.dev;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
